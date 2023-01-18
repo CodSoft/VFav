72667d2ff31718488ed509dd5a0cbd25ce2b370c
rohit-zoxima <rohit.singha@zoxima.com>
2023-01-18 23:30:48 +0530

    structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {NavigationContainer} from '@react-navigation/native';
+
+// Files
+import {persistor, store} from './src/redux/Store';
+import {NavigationRef} from './src/routers/RouterServices';
+import MainStack from './src/routers/MainStack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer ref={NavigationRef}>
+          <StatusBar style="auto" />
+          <MainStack />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
