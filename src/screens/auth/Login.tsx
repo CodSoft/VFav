7a7902d26a05e00165a424feb36594fe6577b612
CodStar <developer.codstar@gmail.com>
2023-01-24 15:22:41 +0530

    login page

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,7 +1,13 @@
-import React, {useMemo} from 'react';
-import {View, StyleSheet, StatusBar, Text} from 'react-native';
-import {useSelector} from 'react-redux';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import React, { useMemo } from 'react';
+import { View, StyleSheet, Image, ImageBackground, Dimensions, Text, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useSelector } from 'react-redux';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+//file
+import Button from '../../components/Buttons/Button';
+
+const window = Dimensions.get('window')
 
 const Login = () => {
   const theme = useSelector((state: any) => state.colors.theme);
@@ -9,9 +15,33 @@ const Login = () => {
 
   return (
     <SafeAreaView edges={['top']} style={styles.screen}>
-      <StatusBar backgroundColor={theme.light} />
+      <StatusBar style="auto" />
       <View style={styles.screen}>
-        <Text>Login</Text>
+        <ImageBackground source={require('../../../assets/images/VFav_LoginPage.png')} style={{ width: '100%', height: '100%' }}>
+          <View style={styles.aboveScreenContainer}>
+            <View style={styles.imageContainer}>
+              <ImageBackground
+                source={require('../../../assets/images/dating_pic2.jpg')}
+                style={styles.imageUpperContainer} />
+            </View>
+          </View>
+          {/* <Image source={require('../../../assets/images/cupid.png')} style={{ width: 200, height: 200, alignSelf: 'center', resizeMode: 'stretch', marginTop: 40 }} /> */}
+          {/* <Text style={{ fontSize: 30, fontWeight: 'bold', alignSelf: 'center', color: 'white' }}>FIND YOUR LOVE</Text> */}
+          <Button label={'Login'}
+            color={['#4c669f', '#3b5998', '#192f6a']}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            ripple={false}
+          />
+          <Button
+            label={'Sign Up'}
+            color={['#4c669f', '#3b5998', '#192f6a']}
+            ripple={true}
+          />
+          <Button label={'Sign'} />
+          <Button color={['#4c669f', '#3b5998', '#192f6a']} ripple={true} />
+
+        </ImageBackground>
       </View>
     </SafeAreaView>
   );
@@ -21,7 +51,26 @@ const createStyles = (theme: any) =>
   StyleSheet.create({
     screen: {
       flex: 1,
-      backgroundColor: theme.white,
+      justifyContent: 'center',
+    },
+    aboveScreenContainer: {
+      height: '30%',
+    },
+    imageContainer: {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderBottomRightRadius: 200,
+      borderBottomLeftRadius: 200,
+      overflow: 'hidden',
+      transform: [{ scaleX: 1.5 }],
+    },
+    imageUpperContainer: {
+      width: '100%',
+      height: '100%',
+      transform: [{ scaleX: .67 }],
     },
   });
 
