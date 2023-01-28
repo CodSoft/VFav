812b80b8f3363bc9f842a3b32cce158b610f8f65
CodStar <developer.codstar@gmail.com>
2023-01-28 18:53:58 +0530

    login-> welcome & code update

--- src/screens/auth/Welcome.tsx
+++ src/screens/auth/Welcome.tsx
@@ -1,15 +1,16 @@
 import React, { useMemo } from 'react';
-import { View, StyleSheet, Image, ImageBackground, Dimensions, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image, ImageBackground, Dimensions, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useSelector } from 'react-redux';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 //file
 import Button from '../../components/Buttons/Button';
+import CurveHeader from '../../components/CurveHeader/CurveHeader';
 
-const window = Dimensions.get('window')
+const window = Dimensions.get('window');
 
-const Welcome = () => {
+const Welcome = ({ navigation }: any) => {
   const theme = useSelector((state: any) => state.colors.theme);
   const styles = useMemo(() => createStyles(theme), [theme]);
 
@@ -18,20 +19,21 @@ const Welcome = () => {
       <StatusBar style="auto" />
       <View style={styles.screen}>
         <ImageBackground source={require('../../../assets/images/VFav_LoginPage.png')} style={{ width: '100%', height: '100%' }}>
-          <View style={styles.aboveScreenContainer}>
-            <View style={styles.imageContainer}>
-              <ImageBackground
-                source={require('../../../assets/images/dating_pic2.jpg')}
-                style={styles.imageUpperContainer} />
-            </View>
-          </View>
-          <Image source={require('../../../assets/images/cupid.png')} style={{ width: '40%', height: '25%', alignSelf: 'center', resizeMode: 'stretch', marginTop: 40 }} />
-          <Text style={{ marginVertical: 10, fontSize: 30, fontWeight: 'bold', alignSelf: 'center', color: 'white', textTransform: 'uppercase', textAlign: 'center' }}>use me or die single !</Text>
+          <CurveHeader source={require('../../../assets/images/dating_pic2.jpg')} />
+          <Image source={require('../../../assets/images/cupid.png')} style={styles.image} />
+          <Text style={styles.text}>use me or die single !</Text>
           <View style={{ marginTop: 30 }}>
-            <Button label={'Log In'}
+            <Button
+              label={'Log In'}
               color={'white'}
-              labelStyle={{ fontSize: 20, color: 'rgba(255,87,87,1)', fontWeight: 'bold' }}
-              onPress={() => { }}
+              labelStyle={{
+                fontSize: 20,
+                color: 'rgba(255,87,87,1)',
+                fontWeight: 'bold',
+              }}
+              onPress={() => {
+                navigation.navigate('Login');
+              }}
             />
             <Button
               label={'Sign Up'}
@@ -40,6 +42,9 @@ const Welcome = () => {
               end={{ x: 0, y: 0 }}
               labelStyle={{ fontSize: 20, color: 'white', fontWeight: 'bold' }}
               containerStyle={{ borderColor: 'white', borderWidth: 1 }}
+              onPress={() => {
+                navigation.navigate('Login');
+              }}
             />
           </View>
         </ImageBackground>
@@ -52,26 +57,22 @@ const createStyles = (theme: any) =>
   StyleSheet.create({
     screen: {
       flex: 1,
-      justifyContent: 'center',
     },
-    aboveScreenContainer: {
-      height: '30%',
+    text: {
+      marginVertical: 10,
+      fontSize: 30,
+      fontWeight: 'bold',
+      alignSelf: 'center',
+      color: 'white',
+      textTransform: 'uppercase',
+      textAlign: 'center',
     },
-    imageContainer: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderBottomRightRadius: 200,
-      borderBottomLeftRadius: 200,
-      overflow: 'hidden',
-      transform: [{ scaleX: 1.5 }],
-    },
-    imageUpperContainer: {
-      width: '100%',
-      height: '100%',
-      transform: [{ scaleX: .67 }],
+    image: {
+      width: '40%',
+      height: '25%',
+      alignSelf: 'center',
+      resizeMode: 'stretch',
+      marginTop: 40,
     },
   });
 
