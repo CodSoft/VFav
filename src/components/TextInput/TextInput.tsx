c16115713402b9db781d2ff4de37a12639880283
CodStar <developer.codstar@gmail.com>
2023-01-28 18:49:39 +0530

    textinput props update

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,13 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import {
-  StyleProp,
-  StyleSheet,
-  ViewStyle,
-  TextInputProps,
-  View,
-  TextInput,
-  Text,
-} from 'react-native';
+import { StyleProp, StyleSheet, ViewStyle, TextInputProps, View, TextInput, Text } from 'react-native';
 import { useSelector } from 'react-redux';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -17,17 +9,13 @@ interface ExtraInputProps {
   iconColor?: string;
   placeholder?: string;
   keyboardType?: TextInputProps['keyboardType'];
+  containerStyle?: StyleProp<ViewStyle>;
+  iconSize?: number;
 }
 
 type InputProps = ExtraInputProps & TextInputProps;
 
-const StyledTextInput: React.FC<InputProps> = ({
-  label,
-  iconName,
-  iconColor,
-  placeholder,
-  keyboardType,
-}) => {
+const StyledTextInput: React.FC<InputProps> = ({ label, iconName, iconColor, placeholder, keyboardType, containerStyle, iconSize }) => {
   const theme = useSelector((state: any) => state.colors.theme);
   const styles = useMemo(() => createStyles(theme), [theme]);
 
@@ -35,26 +23,40 @@ const StyledTextInput: React.FC<InputProps> = ({
   const iconNameValue = iconColor ? iconColor : 'rgba(255,87,87,1)';
   const placeholderValue = placeholder ? placeholder : 'placeholder';
   const keyboardTypeValue = keyboardType ? keyboardType : 'default';
+  const iconSizeValue = iconSize ? iconSize : 24;
+
+  const _defaultPlaceholderValue = () => {
+    switch (labelValue.toLocaleLowerCase()) {
+      case 'email' || 'e-mail':
+        return 'your@email.com';
+      case 'password':
+        return '∗ ∗ ∗ ∗ ∗ ∗ ∗ ∗';
+      case 'username':
+        return 'username@123';
+      default:
+        return 'placeholder';
+    }
+  };
+  const defaultPlaceholderValue = _defaultPlaceholderValue();
 
   const [value, onChangeValue] = useState('');
 
+  // console.log(labelValue.toLocaleLowerCase() === 'password' ? true : false);
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, containerStyle]}>
       <Text style={styles.inputLabel}>{labelValue}</Text>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.input}
           onChangeText={onChangeValue}
           value={value}
-          placeholder="your@email.com"
-          keyboardType="email-address"
+          placeholder={defaultPlaceholderValue}
+          keyboardType={keyboardTypeValue}
+          secureTextEntry={labelValue.toLocaleLowerCase() === 'password' ? true : false}
         />
         <View style={styles.rightIcon}>
-          <MaterialCommunityIcons
-            name={iconName}
-            size={32}
-            color={iconNameValue}
-          />
+          <MaterialCommunityIcons name={iconName} size={iconSizeValue} color={iconNameValue} />
         </View>
       </View>
     </View>
@@ -73,14 +75,14 @@ const createStyles = (theme: any) =>
       // width: '70%',
       height: 50,
       backgroundColor: 'white',
-      borderRadius: 5,
+      borderRadius: 10,
     },
     inputLabel: {
       textTransform: 'uppercase',
       marginBottom: 5,
       fontSize: 15,
       fontWeight: 'bold',
-      color: 'white',
+      color: 'black',
     },
     rightIcon: {
       position: 'absolute',
@@ -97,7 +99,7 @@ const createStyles = (theme: any) =>
       width: '100%',
       borderWidth: 2,
       borderColor: 'rgb(211, 211, 211)',
-      borderRadius: 5,
+      borderRadius: 10,
       paddingLeft: 15,
       paddingRight: '20%',
     },
